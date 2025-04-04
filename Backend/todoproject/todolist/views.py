ddec26237abc106beb490e17b29041dd66f2f835
desireeshortiee <desireeganohay13@gmail.com>
2025-04-04 10:13:34 +0800

    first commit

---
+++ Backend/todoproject/todolist/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from .models import ToDoItem
+from .serializers import ToDoItemSerializer
+
+
+class ToDoItemViewSet(viewsets.ModelViewSet):
+    queryset = ToDoItem.objects.all()
+    serializer_class = ToDoItemSerializer
+
+
